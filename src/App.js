337a4d8af365bd05b441f58879f5c4465bad8e78
Dung Lo <lodung842001@gmail.com>
2022-01-21 11:19:07 +0700

    frustrating

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+
+import NewsCards from "./components/NewsCards/NewsCards";
+
+const alanKey = "29f6f9ce1f7760eb9220133bfc722ca22e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () => {
+    const [newsArticles, setNewsArticles] = useState([]);
+
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({ command, articles }) => {
+                if(command === 'newHeadlines') {
+                    setNewsArticles(articles);
+                }
+            }
+        })
+    }, [])
+
     return (
         <div>
             <h1>Alan AI News Application</h1>
+            <NewsCards articles={newsArticles} />
         </div>
     );
 }
